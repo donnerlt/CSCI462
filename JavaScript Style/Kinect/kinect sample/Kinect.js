e0f92725563ee28593d62db6adf5bc6c8e82c918
Callum Brill <Brillch@g.cofc.edu>
2016-02-17 11:30:05 -0500

    adding the drawing of hand images and serving that image up.

--- JavaScript Style/Kinect/kinect sample/Kinect.js
+++ JavaScript Style/Kinect/kinect sample/Kinect.js
@@ -2,7 +2,8 @@ var Kinect2 = require('kinect2'), express = require('express'), app = express(),
 
 var kinect = new Kinect2();
 
-console.log('burp');
+console.log(process.cwd()+"\\hand.png");
+
 
 if(kinect.open()) {
 server.listen(8000);
@@ -11,6 +12,10 @@ console.log('Point your browser to http://localhost:8000');
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/hand.png', function(req, res) {
+    res.sendFile(__dirname + '/hand.png');
 });
 
 kinect.on('bodyFrame', function(bodyFrame){
