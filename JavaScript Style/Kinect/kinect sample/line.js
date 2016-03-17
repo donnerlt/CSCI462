843b49050af2fdca1ffa5bda77d1655eef4d0ada
Callum Brill <Brillch@g.cofc.edu>
2016-03-17 11:48:36 -0400

    Adding line graph and some more genericization of function calls

--- JavaScript Style/Kinect/kinect sample/line.js
+++ JavaScript Style/Kinect/kinect sample/line.js
@@ -1,1 +1,22 @@
-function fun1(x) {return (((trans*x) + trans2));}
+var message;
+var rise=1;
+var run=1;
+function fun1(x) {return (((rise/run*x) + trans2));}
+
+function getFormula(){
+msg = "y = " + trans+"*"+ x +"+"+ trans2;
+console.log(msg);
+return msg;
+}
+
+//scales graph upward
+function dilation(){
+	rise++;
+	run--;
+}
+
+//scales graph downward
+function contraction(){
+	run++;
+	rise--;
+}
