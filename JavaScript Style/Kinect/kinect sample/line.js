d628634c98787cd145bc1eb98365162e2bb78b35
Callum Brill <Brillch@g.cofc.edu>
2016-04-06 10:17:34 -0400

    Revert "Update line.js"
    This reverts commit f5045d99eb13fa06cbb3e3da94284bb562f2599e.

--- JavaScript Style/Kinect/kinect sample/line.js
+++ JavaScript Style/Kinect/kinect sample/line.js
@@ -4,13 +4,9 @@ var run=1;
 function fun1(x) {return (((rise/run*x) + trans2));}
 
 function getFormula(){
-	if (rise == 1 && run == 1 && trans2 == 0) msg = "y = x";
-	else if (run == 1 && trans2 == 0) msg = "y = " + rise + "x";
-	else if (trans2 == 0) msg = "y = " + rise + "/" + run + "x";
-	else msg = "y = " + rise + "/" + run + "x + " + trans2;
-	//msg = "y = " + trans+"*"+ x +"+"+ trans2;
-	console.log(msg);
-	return msg;
+msg = "y = " + trans+"*"+ x +"+"+ trans2;
+console.log(msg);
+return msg;
 }
 
 //scales graph upward
