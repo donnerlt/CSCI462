da20aa5bc6230a68da582ae1081138573a7f9e79
lilyevans <evanslm2@g.cofc.edu>
2016-04-21 13:15:53 -0400

    Update line.js
    updated formula display

--- JavaScript Style/Kinect/kinect sample/line.js
+++ JavaScript Style/Kinect/kinect sample/line.js
@@ -4,12 +4,37 @@ var run=1;
 function fun1(x) {return (((rise/run*x + trans) + trans2));}
 
 function getFormula(){
-	if (rise == 1 && run == 1 && trans2 == 0 && trans == 0) msg = "y = x";
-	else if (run == 1 && trans2 == 0) msg = "y = " + rise.toFixed(2) + "x + " + trans;
-	else if (trans2 == 0) msg = "y = " + rise.toFixed(2) + "/" + run.toFixed(2) + "x + " + trans;
-	else msg = "y = (" + rise.toFixed(2) + "/" + run.toFixed(2) + "x + " + trans + ") + " + trans2;
+	//if (rise == 1 && run == 1 && trans2 == 0 && trans == 0) msg = "y = x";
+	//else if (run == 1 && trans2 == 0) msg = "y = " + rise.toFixed(2) + "x + " + trans;
+	//else if (trans2 == 0) msg = "y = " + rise.toFixed(2) + "/" + run.toFixed(2) + "x + " + trans;
+	//else msg = "y = (" + rise.toFixed(2) + "/" + run.toFixed(2) + "x + " + trans + ") + " + trans2;
 	//msg = "y = " + trans+"*"+ x +"+"+ trans2;
-	console.log(msg);
+	//console.log(msg);
+	//return msg;
+	
+	var riseRun;
+	var sign;
+	var transDisplay;
+	var transTotal = trans + trans2;
+	
+	// slope portion of formula
+	if (rise/run == 1) riseRun = "";
+	else if (run == 1) riseRun = rise.toFixed(2);
+	else riseRun = "(" + rise.toFixed(2) + "/" + run.toFixed(2) + ")";
+	
+	// y-intercept portion of formula
+	if (transTotal == 0) {
+		sign = "";
+		transDisplay = "";}
+	else if (transTotal > 0) {
+		sign = " + ";
+		transDisplay = transTotal;}
+	else {
+		sign = " - ";
+		transDisplay = transTotal * -1;}
+	
+	// total formula display	
+	msg = "y = " + riseRun + "x" + sign + transDisplay;
 	return msg;
 }
 
